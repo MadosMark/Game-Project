c24478bd69c8362aaf1df982b8de50fae5e1d045
Erik <62840687+nausea87@users.noreply.github.com>
2021-05-11 09:54:28 +0200

    testing branch for implementation of possible fullscreen support

---
+++ src/fullscreenSupport.js
@@ -0,0 +1,24 @@
+// const canvas = document.querySelector("webgl");
+
+// Toggle fullscreen ON / OFF + comp with Safari
+// window.addEventListener("dblclick", () => {
+//   const fullscreenElement =
+//     document.fullscreenElement || document.webkitFullscreenElement;
+
+//   if (!fullscreenElement) {
+//     if (canvas.requestFullscreen) {
+//       canvas.requestFullscreen();
+//     } else if (canvas.webkitRequestFullscreen) {
+//       canvas.webkitRequestFullscreen();
+//     }
+//   } else {
+//     if (document.exitFullscreen) {
+//       document.exitFullscreen();
+//     } else if (document.webkitExitFullscreen) {
+//       document.webkitExitFullscreen();
+//     }
+//   }
+// });
+// END FULLSCREEN
+
+// In progress
